59ce845c399f5dc9dc78851cfe8f8e561c2a7d1a
fmi <fmi@fmi.uni-plovdiv.bg>
2020-01-25 15:37:18 +0200

    adett

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Users from './pages/Users';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div clasName="App">
-    </div>
-   
-  
+    <Router>
+      <Navbar/>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+import { NavLink as RRNavLink } from 'react-router-dom';
+
+const HeaderNavbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="dark" dark expand="md">
+        <div className="container">
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+                <NavItem>
+                    <NavLink 
+                    tag={RRNavLink} 
+                    exact 
+                    to="/" 
+                    activeClassName="active"
+                    >Home</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink 
+                     tag={RRNavLink} 
+                     to="/users" 
+                     activeClassName="active"
+                    >Users</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink 
+                        tag={RRNavLink} 
+                        to="/profile" 
+                        activeClassName="active"
+                        >Profile</NavLink>                
+                </NavItem>
+            </Nav>
+            </Collapse>
+        </div>
+    </Navbar>
+  );
+}
+
+export default HeaderNavbar;
